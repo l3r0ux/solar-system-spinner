99d8807e27a22b4c058c397cd9aaabbe6aeaf1ca
l3r0ux <lerouxvermeulen@gmail.com>
2021-08-22 10:26:40 +0200

    add code for 3D view op solar system as well

---
+++ index.js
@@ -0,0 +1,16 @@
+// const solarSystemContainer = document.getElementById('solar-system-container');
+// const planets = document.querySelectorAll('.celestial-body-shared');
+
+// let solarSytemTransform = 0;
+// let planetTransform = 0;
+
+// document.addEventListener('keydown', (event) => {
+//     if (event.key === 'ArrowDown') {
+//         solarSystemContainer.style.transform = `rotateX(${solarSytemTransform = solarSytemTransform + 5}deg)`;
+//         planets.forEach((planet) => {
+//             planet.style.transform = `rotateX(${planetTransform = planetTransform - 5}deg)`;
+//         })
+//     }
+// })
+
+// up arrow is ArrowUp
