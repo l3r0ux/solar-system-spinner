84d8861680668c1e5bdf2c365c3d5cdd98dca123
l3r0ux <lerouxvermeulen@gmail.com>
2021-09-02 22:37:09 +0200

    make solar system 3d and add generated stars in background

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-// const solarSystemContainer = document.getElementById('solar-system-container');
-// const planets = document.querySelectorAll('.celestial-body-shared');
+const container = document.getElementById('background-stars');
+const colors = ['#97e1ff', '#ffae80', '#6171ff'];
+const shadows = ['#c0edff', '#ffd4bb', '#c0c1ff'];
 
-// let solarSytemTransform = 0;
-// let planetTransform = 0;
+const makeStars = (count) => {
+    for (let i = 0; i < count; i++) {
+        // Generate random top position, select correct colors and randomize size for star
+        const randSelectedColor = Math.floor(Math.random() * colors.length);
+        const randVerticalPos = Math.random() * container.offsetHeight + container.offsetTop;
+        const randSize = Math.floor(Math.random() * (3 - 1) + 1)
 
-// document.addEventListener('keydown', (event) => {
-//     if (event.key === 'ArrowDown') {
-//         solarSystemContainer.style.transform = `rotateX(${solarSytemTransform = solarSytemTransform + 5}deg)`;
-//         planets.forEach((planet) => {
-//             planet.style.transform = `rotateX(${planetTransform = planetTransform - 5}deg)`;
-//         })
-//     }
-// })
+        let horizontalPos = Math.random() * container.offsetWidth + container.offsetLeft;
 
-// up arrow is ArrowUp
+        // Make star
+        const star = document.createElement('span');
+        star.style.width = `${randSize}px`;
+        star.style.height = `${randSize}px`;
+        star.style.backgroundColor = `${colors[randSelectedColor]}`;
+        star.style.boxShadow = `0 0 10px ${shadows[randSelectedColor]}`;
+        star.style.top = `${randVerticalPos}px`;
+        star.style.left = `${horizontalPos}px`;
+        star.classList.add('star');
+        container.append(star)
+    }
+}
+
+makeStars(1000);
