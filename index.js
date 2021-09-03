dc5d0211b860c44df7f672029ef42eec89330aa5
l3r0ux <lerouxvermeulen@gmail.com>
2021-09-03 15:47:12 +0200

    add better play button, new animations, and planets bottom bar

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
+const root = document.querySelector(':root');
 const container = document.getElementById('background-stars');
 const zoomSlider = document.getElementById('zoom-slider');
-const root = document.querySelector(':root');
+const solarSystem = document.getElementById('solar-system')
+const playAnim = document.getElementById('play-button');
+const pauseAnim = document.getElementById('pause-button');
+const planets = document.querySelectorAll('.celestial-body-shared');
 
 const colors = ['#97e1ff', '#ffae80', '#6171ff'];
 const shadows = ['#c0edff', '#ffd4bb', '#c0c1ff'];
+// Default zoom
+let zoom = zoomSlider.value;
 
-const makeStars = (count) => {
+makeStars(800);
+
+root.style.setProperty('--zoom', zoom);
+
+zoomSlider.addEventListener('input', (e) => {
+    zoom = e.target.value;
+    root.style.setProperty('--zoom', zoom);
+})
+
+draggable(solarSystem);
+
+// Playback controls
+playAnim.addEventListener('click', () => {
+    root.style.setProperty('--playState', '');
+    playAnim.classList.toggle('hide');
+    pauseAnim.classList.toggle('hide');
+});
+pauseAnim.addEventListener('click', () => {
+    root.style.setProperty('--playState', 'paused');
+    playAnim.classList.toggle('hide');
+    pauseAnim.classList.toggle('hide');
+})
+
+
+function makeStars(count) {
     for (let i = 0; i < count; i++) {
         // Generate random top position, select correct colors and randomize size for star
         const randSelectedColor = Math.floor(Math.random() * colors.length);
@@ -27,21 +57,6 @@ const makeStars = (count) => {
     }
 }
 
-makeStars(1000);
-
-// Default zoom
-let zoom = zoomSlider.value;
-
-root.style.setProperty('--zoom', zoom);
-
-zoomSlider.addEventListener('input', (e) => {
-    zoom = e.target.value;
-    root.style.setProperty('--zoom', zoom);
-})
-
-const solarSystem = document.getElementById('solar-system')
-draggable(solarSystem);
-
 function draggable(el) {
     let isMouseDown = false;
 
@@ -51,36 +66,24 @@ function draggable(el) {
     let elementX = el.getBoundingClientRect().left;
     let elementY = el.getBoundingClientRect().top;
 
-    el.addEventListener('mousedown', onMouseDown);
-
-    function onMouseDown(e) {
+    el.addEventListener('mousedown', (e) => {
         mouseX = e.clientX;
         mouseY = e.clientY;
         isMouseDown = true;
+    });
 
-        // set all z-indeces to 0
-        document.querySelectorAll('.todo-list').forEach((list) => {
-            list.style.zIndex = 0;
-        })
-        // and put the clicked on to 1 to make it stand out above others
-        this.style.zIndex++
-    }
-
-    el.addEventListener('mouseup', onMouseUp);
-
-    function onMouseUp() {
+    el.addEventListener('mouseup', () => {
         isMouseDown = false;
         elementX = parseInt(el.style.left) || 0;
         elementY = parseInt(el.style.top) || 0;
-    }
+    });
 
-    document.addEventListener('mousemove', onMouseMove);
-
-    function onMouseMove(e) {
+    document.addEventListener('mousemove', (e) => {
         if (!isMouseDown) return;
+        // The Deltas here are the differences in location to where the mouse was moved
         let deltaX = e.clientX - mouseX;
         let deltaY = e.clientY - mouseY;
         el.style.left = elementX + deltaX + 'px';
         el.style.top = elementY + deltaY + 'px';
-    }
+    });
 }
